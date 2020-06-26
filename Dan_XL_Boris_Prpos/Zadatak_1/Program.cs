0f5ea2be419424eb963340207aba74c5f2e41be0
BorisP1996 <prposboris13@hotmail.com>
2020-06-26 14:54:15 +0200

    with delegate

--- Dan_XL_Boris_Prpos/Zadatak_1/Program.cs
+++ Dan_XL_Boris_Prpos/Zadatak_1/Program.cs
@@ -12,6 +12,8 @@ namespace Zadatak_1
         static List<Thread> threadList3 = new List<Thread>();
         static List<Thread> threadList4 = new List<Thread>();
         static Random rnd = new Random();
+        static CountdownEvent countdown1 = new CountdownEvent(1);
+        static CountdownEvent countdown2 = new CountdownEvent(1);
 
         static void Main(string[] args)
         {
@@ -44,12 +46,12 @@ namespace Zadatak_1
             Thread StartA4Thread = new Thread(() => StartA4(threadList4));
 
             StartA3Thread.Start();
-            Thread.Sleep(10);
+            Thread.Sleep(20);
             StartA4Thread.Start();
-           
-           
+
             Console.ReadLine();
         }
+      
         static void StartA3(List<Thread> list3)
         {
             for (int i = 0; i < list3.Count; i++)
@@ -57,6 +59,15 @@ namespace Zadatak_1
                 list3[i].Start();
                 list3[i].Join();
                 Thread.Sleep(100);
+                if (list3[i]==list3[list3.Count-1])
+                {
+                    countdown1.Signal();
+                }
+                if (countdown1.IsSet && countdown2.IsSet)
+                {
+                    Delegate d = new Delegate();
+                    d.Exit();
+                }
             }
         }
         static void StartA4(List<Thread> list4)
@@ -66,7 +77,34 @@ namespace Zadatak_1
                 list4[i].Start();
                 list4[i].Join();
                 Thread.Sleep(100);
+                if (list4[i]==list4[list4.Count-1])
+                {
+                    countdown2.Signal();
+                }
+                if (countdown1.IsSet && countdown2.IsSet)
+                {
+                    Delegate d = new Delegate();
+                    d.Exit();
+                }
             }
+        }
+    }
+    class Delegate
+    {
+        public delegate void Notification();
+
+        public event Notification OnNotification;
+
+        public void Exit()
+        {
+            OnNotification += () =>
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("\nEvery printer has printed.Application will stop after 5 seconds.");
+                Thread.Sleep(5000);
+                Environment.Exit(0);
+            };
+            OnNotification.Invoke();
         }
     }
 }
